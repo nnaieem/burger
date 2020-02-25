53d3b3d7876a23f08051e77860af85ab5e95e8f4
nnaieem <nicholas.naieem@gmail.com>
2020-02-24 21:19:10 -0500

    functional

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,18 +10,29 @@ router.get('/burgers', async (_, res) => {
     res.json(data);
 });
 
-router.put('/burger/:id/devour', async (req, res) =>{
-    const { id } = req.params.id;
+router.post('/burger/new', async (req, res) => {
+    const { name } = req.body
 
-    res.send(id);
+    await db.Burger.create({
+        name,
+        isDevoured: false
+    });
 
-    // await db.Burger.update({
-    //     isDevoured: true
-    // }, {
-    //     id
-    // });
+    res.status(200).end();
+})
 
-    // res.status(200).end();
+router.put('/burger/:id/devour', async (req, res) => {
+    const { id } = req.params;
+
+    await db.Burger.update({
+        isDevoured: true
+    }, {
+        where: {
+            id
+        }
+    });
+
+    res.status(200).end();
 })
 
 module.exports = router;
